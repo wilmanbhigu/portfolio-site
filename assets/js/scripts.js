ad05478d7189b2b29f69f9c7bcc686cc24c5cf53
Andrew Robilliard <robilliardnz@gmail.com>
2018-02-04 22:16:32 +1300

    Semi-implemented post gathering

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,38 +14,28 @@ $(".top").click(function() {
 });
 
 $(document).ready(function () {
-    var about = readTextFile("https://andrewrobilliard.com/assets/blogPosts/about.htm");
+    var about = httpGet("https://andrewrobilliard.com/assets/blogPosts/about.htm");
     if (about != -1){
         $('.content').append(about);
     }
     
     var count = 1;
-    var post = readTextFile("https://andrewrobilliard.com/assets/blogPosts/b"+count+".htm");
-    while(post != -1){
+    var post = httpGet("https://andrewrobilliard.com/assets/blogPosts/b"+count+".htm");
+    while(count <= 1){
         if(count == 1){
             $('.content').append('<h1>Blog</h1>')
         }
-        post = readTextFile("https://andrewrobilliard.com/assets/blogPosts/b"+count+".htm");
-        $('.content').append(allText);
+        post = httpGet("https://andrewrobilliard.com/assets/blogPosts/b"+count+".htm");
+        $('.content').append(post);
         count++;
     }
 });
 
-function readTextFile(file)
+function httpGet(theUrl)
 {
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                return allText;
-            }
-        }
-    }
-    rawFile.send(null);
-    return -1;
+    console.log("GET request placed for url: "+theUrl)
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
 }
